a6b58e9e045cdc0ed81d190a1b9f7664593ae884
Paulo Filho <pcf.feat@gmail.com>
2022-03-05 16:01:24 -0400

    chore: add path mapping to typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
