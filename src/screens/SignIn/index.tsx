695e1f447615117d94d6bdbc58ae2ec2b35d37f2
Paulo Filho <pcf.feat@gmail.com>
2022-03-20 14:57:36 -0400

    feat: add input component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import { Input } from '@components/Input';
 import { Container } from './styles';
 
 export function SignIn() {
   return (
     <Container>
-      <Text>SignIn</Text>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
     </Container>
   );
 }
