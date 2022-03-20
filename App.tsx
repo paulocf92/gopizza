8bd6451707b359906479737cf206218b33bd5add
Paulo Filho <pcf.feat@gmail.com>
2022-03-20 13:15:12 -0400

    chore: add app theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </ThemeProvider>
   );
 }
