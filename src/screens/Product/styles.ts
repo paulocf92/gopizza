0bbed23b05e4c30e81bd4b0c68033f61f2d33060
Paulo Filho <pcf.feat@gmail.com>
2022-03-24 17:05:22 -0400

    chore: add product header

--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,6 +1,39 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+  start: { x: 0, y: 1 },
+  end: { x: 0.5, y: 0.5 },
+}))`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: ${getStatusBarHeight() + 33}px 24px 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `}
+`;
+
+export const DeleteLabel = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.TITLE};
+  `}
 `;
