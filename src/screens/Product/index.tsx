b2d3605ade3942ab7d6cb9c072aa3c9ce4dbc0b8
Paulo Filho <pcf.feat@gmail.com>
2022-03-25 15:36:17 -0400

    feat: add product image

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { Photo } from '@components/Photo';
 
 import { Container, Header, Title, DeleteLabel } from './styles';
 
@@ -17,6 +18,8 @@ export function Product() {
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
+
+      <Photo uri="https://github.com/paulocf92.png" />
     </Container>
   );
 }
