81d67692127b884290eb423882bedc162a3db67f
Paulo Filho <pcf.feat@gmail.com>
2022-03-28 07:07:20 -0400

    chore: add home header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { useTheme } from 'styled-components/native';
 
-import { Container } from './styles';
+import happyEmoji from '@assets/images/happy.png';
+
+import {
+  Container,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+  Header,
+} from './styles';
 
 export function Home() {
-  return <Container />;
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin.</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
 }
