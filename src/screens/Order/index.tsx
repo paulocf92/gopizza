345311753844a842503d20a087926ecbbaf48c95
Paulo Filho <pcf.feat@gmail.com>
2022-04-09 18:02:43 -0400

    chore: load product data in the order screen

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import { Platform } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { Alert, Platform } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
+import { OrderNavigationProps } from '@src/@types/navigation';
 import { PIZZA_TYPES } from '@utils/pizzaTypes';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { RadioButton } from '@components/RadioButton';
+import { ProductProps } from '@components/ProductCard';
 
 import {
   Container,
@@ -23,14 +26,36 @@ import {
   Price,
 } from './styles';
 
+type PizzaResponse = ProductProps & {
+  pizza_sizes: {
+    [key: string]: number;
+  };
+};
+
 export function Order() {
   const [size, setSize] = useState('p');
+  const [pizza, setPizza] = useState<PizzaResponse>({} as PizzaResponse);
 
   const navigation = useNavigation();
+  const route = useRoute();
+  const { id } = route.params as OrderNavigationProps;
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    if (id) {
+      firestore()
+        .collection('pizzas')
+        .doc(id)
+        .get()
+        .then(response => setPizza(response.data() as PizzaResponse))
+        .catch(() =>
+          Alert.alert('Pedido', 'Não foi possível carregar o pedido!'),
+        );
+    }
+  }, [id]);
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
@@ -38,10 +63,10 @@ export function Order() {
         <Header>
           <ButtonBack onPress={handleGoBack} />
         </Header>
-        <Photo source={{ uri: 'https://github.com/paulocf92.png' }} />
+        <Photo source={{ uri: pizza.photo_url }} />
 
         <Form>
-          <Title>Nome da pizza</Title>
+          <Title>{pizza.name}</Title>
           <Label>Selecione um tamanho</Label>
 
           <Sizes>
