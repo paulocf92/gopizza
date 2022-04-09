9aa5443e9d9a238a0e8006e83e96f2e00e61e98a
Paulo Filho <pcf.feat@gmail.com>
2022-04-09 14:44:03 -0400

    chore: add radio button component to order screen

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { Container, Header, BackButton, Photo } from './styles';
+import { RadioButton } from '@components/RadioButton';
+
+import { Container, Header, BackButton, Photo, Sizes } from './styles';
 
 export function Order() {
   return (
@@ -10,6 +12,10 @@ export function Order() {
         <BackButton onPress={() => {}} />
       </Header>
       <Photo source={{ uri: 'https://github.com/paulocf92.png' }} />
+
+      <Sizes>
+        <RadioButton title="Pequeno" selected={false} />
+      </Sizes>
     </Container>
   );
 }
