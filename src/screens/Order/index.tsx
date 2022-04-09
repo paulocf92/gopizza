8397c903776b5ad6399724f1d49e485df3e61bf7
Paulo Filho <pcf.feat@gmail.com>
2022-04-09 15:19:49 -0400

    chore: add order form

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -3,30 +3,68 @@ import { Platform } from 'react-native';
 
 import { PIZZA_TYPES } from '@utils/pizzaTypes';
 
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 import { RadioButton } from '@components/RadioButton';
 
-import { Container, Header, ButtonBack, Photo, Sizes } from './styles';
+import {
+  Container,
+  ContentScroll,
+  Header,
+  ButtonBack,
+  Photo,
+  Sizes,
+  Form,
+  Title,
+  Label,
+  FormRow,
+  InputGroup,
+  Price,
+} from './styles';
 
 export function Order() {
   const [size, setSize] = useState('p');
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} />
-      </Header>
-      <Photo source={{ uri: 'https://github.com/paulocf92.png' }} />
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} />
+        </Header>
+        <Photo source={{ uri: 'https://github.com/paulocf92.png' }} />
 
-      <Sizes>
-        {PIZZA_TYPES.map(item => (
-          <RadioButton
-            key={item.id}
-            title={item.name}
-            onPress={() => setSize(item.id)}
-            selected={size === item.id}
-          />
-        ))}
-      </Sizes>
+        <Form>
+          <Title>Nome da pizza</Title>
+          <Label>Selecione um tamanho</Label>
+
+          <Sizes>
+            {PIZZA_TYPES.map(item => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                onPress={() => setSize(item.id)}
+                selected={size === item.id}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$ 00,00</Price>
+
+          <Button title="Confirmar pedido" />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 }
