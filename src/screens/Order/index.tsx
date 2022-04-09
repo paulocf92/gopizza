8d99acda183e3aa3896b5262fad72b8fc762d014
Paulo Filho <pcf.feat@gmail.com>
2022-04-09 14:01:35 -0400

    chore: add order header

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Header, BackButton } from './styles';
 
 export function Order() {
-  return <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined} />;
+  return (
+    <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+    </Container>
+  );
 }
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import LinearGradient from 'react-native-linear-gradient';
+
+import { ButtonBack } from '@components/ButtonBack';
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+}))`
+  padding: ${getStatusBarHeight() + 34}px 24px 0;
+`;
+
+export const BackButton = styled(ButtonBack)`
+  margin-bottom: 108px;
 `;
