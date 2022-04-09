7f638f449083c12a0b7a5934b8dd4cf08936e96e
Paulo Filho <pcf.feat@gmail.com>
2022-04-09 18:38:18 -0400

    chore: add orders listing

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
+
+import { useAuth } from '@hooks/auth';
 
 import { ItemSeparator } from '@components/ItemSeparator';
-import { OrderCard } from '@components/OrderCard';
+import { OrderCard, OrderProps } from '@components/OrderCard';
 
 import { Container, Header, Title, OrderList } from './styles';
 
 export function Orders() {
+  const { user } = useAuth();
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('orders')
+      .where('waiter_id', '==', user?.id)
+      .onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as OrderProps[];
+
+        setOrders(data);
+      });
+
+    return () => unsubscribe();
+  }, [user?.id]);
+
   return (
     <Container>
       <Header>
@@ -13,9 +37,11 @@ export function Orders() {
       </Header>
 
       <OrderList
-        data={['1', '2', '3']}
-        keyExtractor={item => item}
-        renderItem={({ index }) => <OrderCard index={index} />}
+        data={orders}
+        keyExtractor={item => item.id}
+        renderItem={({ index, item }) => (
+          <OrderCard index={index} data={item} />
+        )}
         ItemSeparatorComponent={() => <ItemSeparator />}
       />
     </Container>
